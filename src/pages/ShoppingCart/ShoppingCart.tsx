d225c37220c57764dffaa14ffd42c26f9c8e6ead
Furkan <furkancantavukcu98@gmail.com>
2023-05-08 23:22:17 +0300

    Features such as seeing the products in the basket and applying coupons have been added.

--- src/pages/ShoppingCart/ShoppingCart.tsx
+++ src/pages/ShoppingCart/ShoppingCart.tsx
@@ -7,24 +7,57 @@ import {
   TableRow,
   TableCell,
   TableBody,
+  TextField,
+  IconButton,
 } from "@mui/material";
+
 import axios from "axios";
 import { ICart, IProduct } from "../../Interfaces/interfaces";
 import { Page404 } from "../Page404";
 import "./ShoppingCart.scss";
+import { Clear } from "@mui/icons-material";
 
 function ShoppingCart() {
   const [cart, setCart] = useState<ICart | null>(null);
   const [products, setProducts] = useState<IProduct[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
+  const [discountCode, setDiscountCode] = useState("");
+  const [discount, setDiscount] = useState(0);
+  const [isDiscountApplied, setIsDiscountApplied] = useState(false);
+
+  const handleApplyDiscount = () => {
+    const isValidDiscountCode = discountCode === "ABC123";
+
+    if (isValidDiscountCode) {
+      setDiscount(0.1);
+      setIsDiscountApplied(true);
+      return;
+    }
+
+    setDiscount(0);
+    setDiscountCode("");
+    alert("Invalid discount code");
+  };
+
+  const cancelDiscount = () => {
+    setDiscount(0);
+    setDiscountCode("");
+    setIsDiscountApplied(false);
+  };
+
+  const totalPrice = products.reduce(
+    (acc, product) => acc + product.price * product.quantity,
+    0
+  );
+  const discountedPrice = totalPrice * (1 - discount);
 
   useEffect(() => {
     async function fetchCart() {
       try {
         setLoading(true);
         const response = await axios.get<ICart>(
-          "https://fakestoreapi.com/carts/1"
+          "https://fakestoreapi.com/carts/1 "
         );
         setCart(response.data);
       } catch (error: any) {
@@ -100,42 +133,84 @@ function ShoppingCart() {
   }
 
   return (
-    <TableContainer>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Image</TableCell>
-            <TableCell>Title</TableCell>
-            <TableCell>Price</TableCell>
-            <TableCell>Quantity</TableCell>
-            <TableCell>Total</TableCell>
-            <TableCell>Action</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {products.map((product) => (
-            <TableRow key={product.id}>
-              <TableCell>
-                <img src={product.image} alt={product.title} height="100" />
-              </TableCell>
-              <TableCell>{product.title}</TableCell>
-              <TableCell>{product.price}₺</TableCell>
-              <TableCell>{product.quantity}</TableCell>
-              <TableCell>{product.price * product.quantity}₺</TableCell>
-              <TableCell>
-                <Button
-                  variant="outlined"
-                  color="secondary"
-                  onClick={() => handleRemoveFromCart(product.id)}
-                >
-                  Remove
-                </Button>
-              </TableCell>
+    <>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Image</TableCell>
+              <TableCell>Title</TableCell>
+              <TableCell>Price</TableCell>
+              <TableCell>Quantity</TableCell>
+              <TableCell>Total</TableCell>
+              <TableCell>Action</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {products.map((product) => (
+              <TableRow key={product.id}>
+                <TableCell>
+                  <img src={product.image} alt={product.title} height="100" />
+                </TableCell>
+                <TableCell>{product.title}</TableCell>
+                <TableCell>{product.price}₺</TableCell>
+                <TableCell>{product.quantity}</TableCell>
+                <TableCell>{product.price * product.quantity}₺</TableCell>
+                <TableCell>
+                  <Button
+                    variant="outlined"
+                    color="secondary"
+                    onClick={() => handleRemoveFromCart(product.id)}
+                  >
+                    Remove
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <div className="pay-container">
+        <div className="price location">
+          <strong>Total Price:</strong>
+          {isDiscountApplied ? (
+            <del>{totalPrice}₺</del>
+          ) : (
+            <span>{totalPrice}₺</span>
+          )}
+        </div>
+        <div className="discount location">
+          <div className="discount-code">
+            <TextField
+              label="Discount Code"
+              value={discountCode}
+              disabled={isDiscountApplied}
+              onChange={(e) => setDiscountCode(e.target.value.toUpperCase())}
+            />
+            {discountCode && isDiscountApplied && (
+              <IconButton onClick={() => cancelDiscount()}>
+                <Clear />
+              </IconButton>
+            )}
+          </div>
+          <Button
+            variant="contained"
+            color="success"
+            onClick={handleApplyDiscount}
+          >
+            Apply
+          </Button>
+        </div>
+        <div className="discount-price location">
+          <strong>Total Price after Discount:</strong>
+          <span>{discountedPrice.toFixed(2)}₺</span>
+        </div>
+        <Button className="buy-button" variant="contained" color="primary">
+          Buy Now
+        </Button>
+      </div>
+    </>
   );
 }
 
